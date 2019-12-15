ee41980dca5284d8fc9e476df7bf89b560131cdd
Avkash <avkaashsharma89@gmail.com>
2019-12-14 21:13:08 -0500

    Added input in app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 
-print("1 - Test Data")
-print("2 - Training Data")
+print('Enter Attribute: 0 - CPUUtilization_Average, 1 - NetworkIn_Average, 2 - NetworkOut_Average, 3 - MemoryUtilization_Average')
+attribute_entered = input()
+print('Enter Datatype: 0 - Training , 1 - Testing')
+datatype_entered = input()
 
+print(attribute_entered)
+print(datatype_entered)
 
-n= 0
+n = 0
 
 while True:
     n+1
-
-
